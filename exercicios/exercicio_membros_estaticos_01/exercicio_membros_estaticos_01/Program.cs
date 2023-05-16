930ca4b97b4009142e8a5212c6ab2712161bf6a2
Diego Gomes <diegogns18@outlook.com>
2023-05-16 10:41:45 -0300

    membros estaticos

---
+++ exercicios/exercicio_membros_estaticos_01/exercicio_membros_estaticos_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using exercicio_membros_estaticos_01;
+using System.Globalization;
+
+Console.Write("Qual é a cotação do dólar? ");
+double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantos dólares você vai comprar? ");
+double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
