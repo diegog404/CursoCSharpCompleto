bfff6f1f7b2fb015c2d94f6d46b7bc9a878d2e47
Diego Gomes <diegogns18@outlook.com>
2023-05-18 20:33:33 -0300

    Revisão vetores com OO

---
+++ exercicios/exercicio_revisao_vetores/exercicio_revisao_vetores/Aluguel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_revisao_vetores
+{
+    internal class Aluguel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Aluguel(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome+ ",  " + Email;
+        }
+    }
+}
---
+++ exercicios/exercicio_revisao_vetores/exercicio_revisao_vetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using exercicio_revisao_vetores;
+
+Aluguel[] quartos = new Aluguel[10];
+
+Console.Write("How many rooms will be rented? ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("Rent #" + i);
+
+    Console.Write("Name: ");
+    string nome = Console.ReadLine();
+
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+
+    Console.Write("Room: ");
+    int quarto = int.Parse(Console.ReadLine());
+
+    quartos[quarto] = new Aluguel(nome, email);
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Busy rooms:");
+for(int i = 0; i < 10; i++)
+{
+    if (quartos[i] != null)
+    {
+        Console.WriteLine(i + ": " + quartos[i]);
+    }
+}
