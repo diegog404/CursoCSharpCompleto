a98d3f93580c22036e57e81ec5b7d8e95c98a3c9
Diego Gomes <diegogns18@outlook.com>
2023-05-26 19:50:21 -0300

    Enumeracoes

---
+++ exemplos/Enumeracoes/Enumeracoes/Program.cs
@@ -0,0 +1,11 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
