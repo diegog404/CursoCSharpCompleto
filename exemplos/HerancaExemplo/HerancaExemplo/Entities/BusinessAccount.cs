b60e3ad0d334d3fdfca0bfa908940125d78448b7
Diego Gomes <diegogns18@outlook.com>
2023-06-07 11:05:31 -0300

    teoria e exemplos de Herança

---
+++ exemplos/HerancaExemplo/HerancaExemplo/Entities/BusinessAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaExemplo.Entities
+{
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        //base passa o construtor da superclasse pra reaproveitar código
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            loanLimit = LoanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }            
+        }
+    }
+}
