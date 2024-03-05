f60158793b3d613763b3251f2a1899441aa171f2
Diego Gomes <diegogns18@outlook.com>
2024-03-05 14:24:47 -0300

    Revisão exercício enums e composições

---
+++ exercicios/ExercicioEnumComposicao/ExercicioEnumComposicao/Entities/Order.cs
@@ -0,0 +1,69 @@
+﻿using ExercicioEnumComposicao.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioEnumComposicao.Entities
+{
+    internal class Order
+    {
+        public DateTime Momment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Clients { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime momment, OrderStatus status, Client clients)
+        {
+            Momment = momment;
+            Status = status;
+            Clients = clients;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("ORDER SUMMARY:");
+            sb.AppendLine("Order moment: " + Momment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Clients.Name + " (" + Clients.BirthDate.ToString("dd/MM/yyyy") + ") - " + Clients.Email);
+            sb.AppendLine("Order items:");
+
+            foreach (OrderItem oi in Items)
+            {
+                sb.AppendLine(oi.Product.Name + ", " + oi.Price.ToString("F2", CultureInfo.InvariantCulture)
+                    + ", Quantity: " + oi.Quantity + ", SubTotal: " + oi.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ exercicios/ExercicioEnumComposicao/ExercicioEnumComposicao/Program.cs
@@ -0,0 +1,58 @@
+﻿using ExercicioEnumComposicao.Entities;
+using ExercicioEnumComposicao.Enums;
+using System.Globalization;
+using System.Xml.Linq;
+
+Console.WriteLine("Enter client data:");
+Console.WriteLine();
+
+Console.WriteLine("Name: ");
+string name = Console.ReadLine();
+
+Console.Write("Email: ");
+string email = Console.ReadLine();
+
+Console.Write("Birth date (DD/MM/YYYY): ");
+DateTime date = DateTime.Parse(Console.ReadLine());
+
+Client client = new Client(name, email, date);
+Console.WriteLine();
+
+Console.WriteLine("Enter order data:");
+
+Console.Write("Status: ");
+OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+Console.WriteLine();
+
+Order order = new Order(DateTime.Now, status, client);
+
+Console.Write("How many items to this order? ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Enter #{i} item data:");
+    Console.WriteLine();
+
+    Console.Write("Product name: ");
+    string prodName = Console.ReadLine();
+
+    Console.Write("Product price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Console.Write("Quantity: ");
+    int quantity = int.Parse(Console.ReadLine());
+
+    Product prod = new Product(prodName, price);
+    OrderItem orderItem = new OrderItem(quantity, price, prod);
+    
+    order.AddItem(orderItem);
+}
+
+Console.WriteLine("ORDER SUMMARY:");
+Console.WriteLine(order);
+Console.WriteLine("Total price: $" + order.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
